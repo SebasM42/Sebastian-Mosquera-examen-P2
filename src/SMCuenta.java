4fc84c16df9f2ee32eabd9883895f9562d8a7ffd
SebasM42 <sebas121024m@gmail.com>
2025-05-29 16:13:20 -0500

    Primer commit

---
+++ src/SMCuenta.java
@@ -0,0 +1,24 @@
+public class SMCuenta {
+    protected double saldo;
+    public SMCuenta(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+        }
+    }
+    public boolean retirar(double monto) {
+        if (monto > 0 && monto <= saldo) {
+            saldo -= monto;
+            return true;
+        }
+        return false;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+
+
+}
