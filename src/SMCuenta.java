c2af1abfa9cddacf9d13d96b1fa7013719edbe19
SebasM42 <sebas121024m@gmail.com>
2025-05-29 17:05:56 -0500

    octavo commit

--- src/SMCuenta.java
+++ src/SMCuenta.java
@@ -10,4 +10,26 @@ public class SMCuenta {
         this.numeroRetiros = 0;
         this.tasaAnual = 0.0;
     }
+
+    public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+            numeroDepositos++;
+        }
+    }
+
+    public boolean retirar(double monto) {
+        if (monto > 0 && monto <= saldo) {
+            saldo -= monto;
+            numeroRetiros++;
+            return true;
+        }
+        return false;
+    }
+
+    public String imprimir() {
+        return String.format(
+                "Saldo: %.2f\nNúmero de depósitos: %d\nNúmero de retiros: %d\nTasa anual: %.2f%%", saldo, numeroDepositos, numeroRetiros, tasaAnual
+        );
+    }
 }
--- src/SMCuentaAhorro.java
+++ src/SMCuentaAhorro.java
@@ -2,26 +2,4 @@ public class SMCuentaAhorro extends SMCuenta {
     public SMCuentaAhorro(double saldoInicial) {
         super(saldoInicial);
     }
-
-    public void depositar(double monto) {
-        if (monto > 0) {
-            saldo += monto;
-            numeroDepositos++;
-        }
-    }
-
-    public boolean retirar(double monto) {
-        if (monto > 0 && monto <= saldo) {
-            saldo -= monto;
-            numeroRetiros++;
-            return true;
-        }
-        return false;
-    }
-
-    public String imprimir() {
-        return String.format(
-                "Saldo: %.2f\nNúmero de depósitos: %d\nNúmero de retiros: %d\nTasa anual: %.2f%%", saldo, numeroDepositos, numeroRetiros, tasaAnual
-        );
-    }
 }
